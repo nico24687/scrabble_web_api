953a708d6a4d15ce6047d46e9cc3de9a15f5b65c
nico24687 <theonlynico@hotmail.com>
2018-02-21 11:09:18 -0700

    now showing the game_id when you visit /api/v1/games/1

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,5 +1,18 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes  :game_id, :scores 
 
-  has_many :plays
+  # has_many :plays
+
+  def game_id
+    object.id
+  end
+
+  # def scores
+  #   [object.attributes.merge(score: 15)]
+  # end
+
+  def scores 
+    object.attributes
+  end
+
 end
