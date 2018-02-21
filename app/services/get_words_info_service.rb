108fdf30a10c0f02817d68806f17920dd43307bc
nico24687 <theonlynico@hotmail.com>
2018-02-21 10:13:15 -0700

    get words service spec now passing

--- app/services/get_words_info_service.rb
+++ app/services/get_words_info_service.rb
@@ -1,5 +1,18 @@
-class GetWordsInfoService do 
+class GetWordsInfoService
+
   def find_words(word)
-    
+    app_id = ENV['app_id']
+    app_key = ENV['app_key']
+
+    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
+      faraday.headers["app_id"] = "4af01898"
+      faraday.headers["app_key"] = "4519fdd4a9798d8e03482f48f1e4a389"
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = @conn.get("/api/v1/inflections/en/#{word}")
+
+    JSON.parse(response.body)
+
   end
 end 
--- spec/services/get_words_info_service_spec.rb
+++ spec/services/get_words_info_service_spec.rb
@@ -4,7 +4,7 @@ describe GetWordsInfoService do
   it "returns the correct word information for a given word" do 
     word = "foxes"
     words = GetWordsInfoService.new.find_words(word)
-
-    
+  
+    expect(words["results"].first["lexicalEntries"].first["inflectionOf"].first).to have_key("text")
   end
 end 
