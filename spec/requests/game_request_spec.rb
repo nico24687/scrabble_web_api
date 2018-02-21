51ae8ef65630579a3635e5816b963f89c33d1b08
nico24687 <theonlynico@hotmail.com>
2018-02-21 10:49:51 -0700

    first request test passing but need to implement serializers

--- spec/requests/game_request_spec.rb
+++ spec/requests/game_request_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 describe "Games API" do 
   it "gets a particular game" do 
-    game = Game.create(player_1_id: 1, player_2_id: 2)
+    game = create(:game)
 
     get "/api/v1/games/#{game.id}"
 
     game = JSON.parse(response.body)
-    binding.pry
-    expect(game).to have_key("game_id")
+    expect(game).to have_key("id")
   end
 end
 
