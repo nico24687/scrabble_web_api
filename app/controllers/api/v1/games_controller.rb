8109f0c49f5726b7441472d86d58a97697274540
nico24687 <theonlynico@hotmail.com>
2018-02-21 11:22:14 -0700

    finishing up third feature test

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -5,4 +5,12 @@ class Api::V1::GamesController < ApplicationController
   def show 
     render json: Game.find(params[:id])
   end
+  def create 
+    render json: Game.create(game_params)
+  end
+
+  private 
+    def game_params
+      params.require(:game).permit(:player_1_id, :player_2_id)
+    end
 end
