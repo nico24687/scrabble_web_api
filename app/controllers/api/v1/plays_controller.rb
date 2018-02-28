839957648e08f507437fdc6979c2732ca65215b3
nico24687 <theonlynico@hotmail.com>
2018-02-27 19:42:06 -0700

    finished the last itteration

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::PlaysController < ApplicationController 
-  
+  before_action :ensure_valid_word, only: :create
+
   def create 
     play = Play.new(play_params)
     play.game_id = params[:game_id].to_i
@@ -11,4 +12,11 @@ class Api::V1::PlaysController < ApplicationController
     def play_params
       params.require(:play).permit(:user_id, :word)
     end
+
+    def ensure_valid_word
+      word = WordPresenter.new(play_params[:word]).word
+      unless word.valid? 
+        render json: {"message": "#{play_params[:word]} is not a valid word."}, status: 422 
+      end
+    end 
 end
--- spec/requests/play_request_spec.rb
+++ spec/requests/play_request_spec.rb
@@ -2,14 +2,32 @@ require 'rails_helper'
 
 describe "plays API" do 
   it "can create a play" do 
-    game = create(:game)
-    play_params = {user_id: game.player_1.id, word: "at"}
-    post "/api/v1/games/#{game.id}/plays", params: {play: play_params}
+    VCR.use_cassette("services/get_words_info_service") do 
+      game = create(:game)
+      play_params = {user_id: game.player_1.id, word: "foxes"}
+      post "/api/v1/games/#{game.id}/plays", params: {play: play_params}
 
-    play = Play.last 
-    expect(response).to have_http_status(201)
-    expect(play.user_id).to eq(game.player_1.id)
-    expect(play.word).to eq("at")
+      play = Play.last 
+      expect(response).to have_http_status(201)
+      expect(play.user_id).to eq(game.player_1.id)
+      expect(play.word).to eq("foxes")
+    end 
+  end
+  context "when the word is invalid" do 
+    it "gives an error message" do 
+
+      VCR.use_cassette("services/get_words_info_service_error") do 
+        game = create(:game)
+        play_params = {user_id: game.player_1.id, word: "foxez"}
+        
+        post "/api/v1/games/#{game.id}/plays", params: {play: play_params}
+
+        play = Play.last
+        expect(response).to have_http_status(422)
+        expect(JSON.parse(response.body)["message"]).to eq("foxez is not a valid word.")
+      end 
+
+    end
   end
 end
 
